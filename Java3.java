2cd851ac433e71b7025fa46bdeab624a08f9082e
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:55:02 +0530

    Add files via upload

---
+++ Java3.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java3;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(10+2);
+        System.out.println("Girls"+20+20);
+        System.out.println(20+20+"Boys");
+        
+    }
+    
+}
