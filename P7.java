66ba9a9368256d3975b72a5a89028cd41018dae9
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:49:50 +0530

    Add files via upload

---
+++ P7.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p7;
+
+/**
+ *
+ * @author SMV
+ */
+public class P7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j==0||i==0||i==n/2){
+                    System.out.print("*"+" ");
+            }else{
+                    System.out.print(" "+" ");
+                }}
+            System.out.println();
+        }}}
+        
+        // TODO code application logic here
+    
+    
+
