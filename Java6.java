1eaff95158be6fa3dae70ae53f83b37ad5cbfa8c
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:55:54 +0530

    Add files via upload

---
+++ Java6.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java6;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int ml=30;
+        System.out.println(ml);
+         ml=60;
+        System.out.println(ml);
+        ml=90;
+        System.out.println(ml);
+    }
+    
+}
