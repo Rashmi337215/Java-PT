6939070c82dd1c9d00c498a4145bcf530713cb56
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:55:34 +0530

    Add files via upload

---
+++ Java4.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java4;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int val=10;
+        double num=11.03;
+        char ch='r';
+        boolean res=true;
+        String name="Rashmi";
+        System.out.println(val);
+        System.out.println(num);
+        System.out.println(ch);
+        System.out.println(res);
+        System.out.println(name);
+    }
+    
+}
