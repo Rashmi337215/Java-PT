14e6a598dd951060b53b7efef300dc15b5621d5f
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:48:00 +0530

    Add files via upload

---
+++ P6.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p6;
+
+/**
+ *
+ * @author SMV
+ */
+public class P6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j==0||i==0||i==n/2||i==n-1){
+                    System.out.print("*"+" ");
+            }else{
+                    System.out.print(" "+" ");
+                }}
+            System.out.println();
+        }}}
+        
+        // TODO code application logic here
+    
+    
