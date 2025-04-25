793eb4c060baec4bfaa50c2ab3986f4ca2897ff0
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:43:26 +0530

    Add files via upload

---
+++ Prog001.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog001;
+
+/**
+ *
+ * @author SMV
+ */
+public class Prog001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int x=7;
+       do
+       {
+           System.out.println("java");
+           x--;
+       }
+       while(x<5);
+    }
+}
+            
+         
+         
+             
+         
+             
+             
+     
