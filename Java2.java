c81266d5405d9f351fb6c3bc45008a333acddc35
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:54:23 +0530

    Add files via upload

---
+++ Java2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java2;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Rashmi");
+         System.out.println('S');
+          System.out.println(2025);
+           System.out.println(13.99);
+            System.out.println(true);
+             System.out.println(false);
+    }
+    
+}
