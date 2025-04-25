ff1a8ac4f7b13e873a9b5ded57c02c86053e6c8c
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:48:31 +0530

    Add files via upload

---
+++ P07.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p07;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SMV
+ */
+public class P07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+      int a=scn.nextInt();
+      int b=scn.nextInt();
+      while(b>0){
+          a++;
+          b--;
+      }
+      System.out.println(a);
+    }
+}
+
+      
+      
+      
+      
+        
+        
+        
+        
+        
+        
+    
+        // TODO code application logic here
+       
+    
+    
+
