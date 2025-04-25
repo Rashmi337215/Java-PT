144476464ca2495774f21a79235eaf5a62d89478
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:34:36 +0530

    Add files via upload

---
+++ Prog1.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog1;
+
+/**
+ *
+ * @author SMV
+ */
+public class Prog1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=10;
+        int b=2;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a=b);
+        System.out.println(10+5-2);
+        System.out.println(10-5+5);
+        System.out.println(10*2/2);
+        System.out.println(10/2+5*2);
+        System.out.println(4*3+7*2);
+        System.out.println(15/3*4);
+        System.out.println(10/2*6/3);
+        System.out.println(10+2*5+3);
+    }
+    }
+    
+
+
