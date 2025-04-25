ec55534cada5f60e25cd8d23223d37fa9a5cf61e
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:46:50 +0530

    Add files via upload

---
+++ P3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p3;
+
+/**
+ *
+ * @author SMV
+ */
+public class P3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||j==0||i==n/2||j==n-1){
+                    System.out.print("*"+" ");
+            }else{
+                    System.out.print(" "+" ");
+                }}
+            System.out.println();
+        }}}
+        
+        // TODO code application logic here
+    
+    
+
