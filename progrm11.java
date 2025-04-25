dae63fb931210082905cd2f277b09b6fae63af0c
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:42:01 +0530

    Add files via upload

---
+++ progrm11.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog8;
+
+/**
+ *
+ * @author SMV
+ */
+public class progrm11 {
+    public static void main(String[]args){
+       
+    for(int i=0;i<5;i++){
+        if(i%2==0){
+            System.out.print("X"+" ");
+            
+        }
+        else
+        {
+     
+        System.out.print("O"+" ");
+        }
+    }
+    }    
+    }
+    
+
