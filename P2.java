502718a3d57e033a0d3e1b13ef06ad67ed70ce42
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:45:35 +0530

    Add files via upload

---
+++ P2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p2;
+
+/**
+ *
+ * @author SMV
+ */
+public class P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int row=5;
+        for(int i=row;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+        // TODO code application logic here
+        
+
