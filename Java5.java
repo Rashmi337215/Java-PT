d8d5dd71632f85338e9c42ca041c867609ec7b22
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:56:16 +0530

    Add files via upload

---
+++ Java5.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java5;
+
+/**
+ *
+ * @author SMV
+ */
+public class Java5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        long aadharnumber=123456789124L;
+        float marks=13.66f;
+        System.out.println(aadharnumber+" "+marks);
+        
+    }
+    
+}
