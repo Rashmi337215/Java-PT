b3da6adc2ea85e50759f1ebda1d499dcb4453ed2
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-04-25 09:41:02 +0530

    Add files via upload

---
+++ prog14.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog8;
+
+/**
+ *
+ * @author SMV
+ */
+public class prog14 {
+    public static void main(String[]args){
+        
+        System.out.println("Program Starts");
+        int n=5;
+        while(n>0)
+        {
+            System.out.println("LIM");
+            n--;
+        }
+        System.out.println("Program ends");
+        
+        }
+    }
+    
+
