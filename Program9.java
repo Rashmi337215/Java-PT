e3668d9200017c8e952e527aae1648e70b5ea4d7
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-05-02 10:10:16 +0530

    Add files via upload

---
+++ Program9.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program9;
+
+/**
+ *
+ * @author SMV
+ */
+public class Program9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s1="java developer";
+        String s2="Java Tester";
+        System.out.println(s1.substring(2));
+        System.out.println(s1.substring(0,6));
+        System.out.println(s2.trim());
+    
+}
+}
