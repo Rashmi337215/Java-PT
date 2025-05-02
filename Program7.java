ad70913dfa8c0c66835657eedf2777b9a9de712c
Rashmi337215 <rashmi.23cs115@sode-edu.in>
2025-05-02 09:55:31 +0530

    Add files via upload

---
+++ Program7.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package program7;
+
+/**
+ *
+ * @author SMV
+ */
+public class Program7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s="Java Developer";
+        System.out.println(s.charAt(3));
+        System.out.println(s.length());
+         System.out.println(s.toLowerCase());
+          System.out.println(s.toUpperCase());
+    }
+    
+}
